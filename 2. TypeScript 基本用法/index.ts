5b706484ad0a68ff1e038d0c25a2f76d069513ec
YorkWong <isyork@126.com>
2024-09-19 17:32:32 +0800

    ts简介

---
+++ 2. TypeScript 基本用法/index.ts
@@ -0,0 +1,13 @@
+// 1. 类型声明
+// 冒号 + 类型
+let num: number = 10
+const toBeString = (num: number): string => {
+    return String(num)
+}
+console.log(toBeString(num))
+
+// 2. 类型推断
+let num2  = 10
+num2 = '10' // 报错: TypeScript 会自己推断类型
+
+//学习 TypeScript 需要分清楚“值”（value）和“类型”（type）。
